74ff5170675c43a06a54f11f32d0684b98c30e81
malcolmjgruber@gmail.com <malcolmjgruber@gmail.com>
2018-03-12 01:48:23 -0500

    Not so simple character

---
+++ MonkeyKong/Assets/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Transform LeftBounds;
+    public Transform RightBounds;
+
+    public float SmoothDampTime = 0.15f;
+    private Vector3 mSmoothDampVelocity = Vector3.zero;
+
+    private float mCameraWidth, mCameraHeight, mLevelMinX, mLevelMaxX;
+
+    // Use this for initialization
+    void Start()
+    {
+        mCameraHeight = Camera.main.orthographicSize * 2f;
+        mCameraWidth = mCameraHeight * Camera.main.aspect;
+
+        float leftBoundsWidth = LeftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2f;
+        float rightBoundsWidth = RightBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2f;
+
+        mLevelMinX = LeftBounds.position.x + leftBoundsWidth + (mCameraWidth / 2f);
+        mLevelMaxX = RightBounds.position.x - rightBoundsWidth - (mCameraWidth / 2f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Target)
+        {
+            float targetX = Mathf.Max(mLevelMinX, Mathf.Min(mLevelMaxX, Target.position.x));
+            float x = Mathf.SmoothDamp(transform.position.x, targetX, ref mSmoothDampVelocity.x, SmoothDampTime);
+
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ MonkeyKong/Assets/NotSoSimpleController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotSoSimpleController : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    public float JumpSpeed = 5f;
+
+    Rigidbody2D mRigidBody = null;
+    bool mWalking, mWalkingLeft, mWalkingRight, mJump;
+
+    // Use this for initialization
+    void Start()
+    {
+        mRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+
+        UpdatePlayer();
+    }
+
+    void CheckInput()
+    {
+        bool inputLeft = Input.GetKey(KeyCode.A);
+        bool inputRight = Input.GetKey(KeyCode.D);
+        bool inputJump = Input.GetKey(KeyCode.Space);
+
+        mWalking = inputLeft || inputRight;
+        mWalkingLeft = inputLeft && !inputRight;
+        mWalkingRight = !inputLeft && inputRight;
+        mJump = inputJump;
+    }
+
+    void UpdatePlayer()
+    {
+        Vector3 pos = transform.localPosition;
+        Vector3 scale = transform.localScale;
+
+        if (mWalking)
+        {
+            if (mWalkingLeft)
+            {
+                pos.x -= MoveSpeed * Time.deltaTime;
+                scale.x = -1;
+            }
+
+            if (mWalkingRight)
+            {
+                pos.x += MoveSpeed * Time.deltaTime;
+                scale.x = 1;
+            }
+        }
+
+        transform.localPosition = pos;
+        transform.localScale = scale;
+    }
+}
