f07f862c0f586fbb41a66268f6f314bfb3e74d4a
malcolmjgruber@gmail.com <malcolmjgruber@gmail.com>
2018-03-16 01:23:25 -0500

    Jumping with gravity and colliding with floors

--- MonkeyKong/Assets/Scripts/PlayerController.cs
+++ MonkeyKong/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     public float MoveSpeed = 5f;
     public float JumpSpeed = 5f;
+    public float Gravity = 9.8f;
+    public LayerMask CollisionLayer;
 
     Rigidbody2D mRigidBody = null;
     bool mWalking, mWalkingLeft, mWalkingRight, mJump;
+
+    private float mWidth, mHeight;
+    private Vector3 mPosition;
+    private Vector2 mVelocity;
+    private Vector2 mDeltaVelocity;
+    private bool mGrounded;
 
     // Use this for initialization
     void Start()
     {
         mRigidBody = GetComponent<Rigidbody2D>();
+        mWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        mHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+
+        Fall();
     }
 
     // Update is called once per frame
@@ -38,25 +50,110 @@ public class PlayerController : MonoBehaviour
 
     void UpdatePlayerPosition()
     {
-        Vector3 pos = transform.localPosition;
+        mPosition = transform.localPosition;
         Vector3 scale = transform.localScale;
+
+        mVelocity.x = 0f;
 
         if (mWalking)
         {
             if (mWalkingLeft)
             {
-                pos.x -= MoveSpeed * Time.deltaTime;
+                mVelocity.x -= MoveSpeed;
                 scale.x = -1;
             }
 
             if (mWalkingRight)
             {
-                pos.x += MoveSpeed * Time.deltaTime;
+                mVelocity.x += MoveSpeed;
                 scale.x = 1;
             }
         }
 
-        transform.localPosition = pos;
+        mDeltaVelocity.x = mVelocity.x * Time.deltaTime;
+        mPosition.x += mVelocity.x * Time.deltaTime;
+
+        // Handle jumping and falling
+        CheckTopBottomCollisions();
+
+        if (mJump && mGrounded)
+        {
+            mVelocity.y += JumpSpeed;
+            mGrounded = false;
+        }
+
+        if(!mGrounded)
+        {
+            mPosition.y += mVelocity.y * Time.deltaTime;
+
+            mVelocity.y -= Gravity * Time.deltaTime;
+        }
+
+        mDeltaVelocity.y = mVelocity.y * Time.deltaTime;
+
+        // Update position
+        transform.localPosition = mPosition;
         transform.localScale = scale;
+    }
+
+    void Fall()
+    {
+        mVelocity.y = 0;
+
+        mGrounded = false;
+    }
+
+    void HandleGroundCollisions()
+    {
+        Vector2 velocity;
+        velocity.y = mVelocity.y < 0f ? mDeltaVelocity.y : -mHeight;
+
+        Vector2 midFallPoint = new Vector2(mPosition.x, mPosition.y + (velocity.y / 2f));
+        Vector2 boxSize = new Vector2(mWidth, Mathf.Abs(velocity.y) + mHeight);
+
+        Collider2D collisions = Physics2D.OverlapBox(midFallPoint, boxSize, 0f, CollisionLayer);
+
+        if (collisions)
+        {
+            mGrounded = true;
+            if (mVelocity.y < 0f)
+            {
+                mVelocity.y = 0f;
+                mPosition.y = collisions.bounds.ClosestPoint(mPosition).y + mHeight/2f;
+            }
+        }
+        else
+        {
+            mGrounded = false;
+        }
+    }
+
+    void CheckTopBottomCollisions()
+    {
+        if(mVelocity.y > 0)
+        {
+            // Check for ceiling
+        }
+        else
+        {
+            HandleGroundCollisions();
+        }
+    }
+
+
+    // Move this to extend the debug class
+    void DrawDebugBox(Vector2 _point, Vector2 _size)
+    {
+        float halfWidth = _size.x / 2f;
+        float halfHeight = _size.y / 2f;
+        Vector3 topLeft = new Vector3(_point.x - halfWidth, _point.y + halfHeight);
+        Vector3 bottomLeft = new Vector3(_point.x - halfWidth, _point.y - halfHeight);
+        Vector3 topRight = new Vector3(_point.x + halfWidth, _point.y + halfHeight);
+        Vector3 bottomRight = new Vector3(_point.x + halfWidth, _point.y - halfHeight);
+
+        Debug.DrawLine(topLeft, topRight, Color.green);
+        Debug.DrawLine(topRight, bottomRight, Color.green);
+        Debug.DrawLine(bottomRight, bottomLeft, Color.green);
+        Debug.DrawLine(bottomLeft, topLeft, Color.green);
     }
 }
