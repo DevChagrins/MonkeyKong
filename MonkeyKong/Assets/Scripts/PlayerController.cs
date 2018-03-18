dc3aa64ccf2010dd0f204c8d3057e90285c97811
Sparrow <mel.e.grigg@gmail.com>
2018-03-18 17:12:16 -0500

    Wall collision for player

--- MonkeyKong/Assets/Scripts/PlayerController.cs
+++ MonkeyKong/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     public float MoveSpeed = 5f;
     public float JumpSpeed = 5f;
+	public LayerMask CollisionLayer;
 
     Rigidbody2D mRigidBody = null;
     bool mWalking, mWalkingLeft, mWalkingRight, mJump;
+
+	float playerHeight;
+	float playerWidth;
 
     // Use this for initialization
     void Start()
     {
         mRigidBody = GetComponent<Rigidbody2D>();
+
+		float playerHeight = GetComponent<SpriteRenderer> ().bounds.size.y;
+		float playerWidth = GetComponent<SpriteRenderer> ().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -45,14 +52,44 @@ public class PlayerController : MonoBehaviour
         {
             if (mWalkingLeft)
             {
-                pos.x -= MoveSpeed * Time.deltaTime;
-                scale.x = -1;
+				
+				Vector2 newPoint = new Vector2 (pos.x - MoveSpeed * Time.deltaTime, pos.y);
+
+				float width = newPoint.x - pos.x + playerWidth;
+				float height = playerHeight;
+
+				Vector2 newSize = new Vector2 (width, height);
+
+				float angle = 0;
+
+				int layerMask = CollisionLayer.value;
+
+				if (!Physics2D.OverlapBox(newPoint, newSize, angle, layerMask)) {
+					
+					pos.x = newPoint.x;
+					scale.x = -1;
+				}
             }
 
             if (mWalkingRight)
             {
-                pos.x += MoveSpeed * Time.deltaTime;
-                scale.x = 1;
+                
+				Vector2 newPoint = new Vector2 (pos.x + MoveSpeed * Time.deltaTime, pos.y);
+
+				float width = newPoint.x - pos.x + playerWidth;
+				float height = playerHeight;
+
+				Vector2 newSize = new Vector2 (width, height);
+
+				float angle = 0;
+
+				int layerMask = CollisionLayer.value;
+
+				if (!Physics2D.OverlapBox(newPoint, newSize, angle, layerMask)) {
+
+					pos.x = newPoint.x;
+					scale.x = 1;
+				}
             }
         }
 
