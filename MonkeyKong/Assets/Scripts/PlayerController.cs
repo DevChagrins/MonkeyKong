4410b421f1e123b4c82ad52fb38163b803ad09a1
malcolmjgruber@gmail.com <malcolmjgruber@gmail.com>
2018-03-18 20:59:02 -0500

    Player changes

--- MonkeyKong/Assets/Scripts/PlayerController.cs
+++ MonkeyKong/Assets/Scripts/PlayerController.cs
@@ -16,17 +16,23 @@ public class PlayerController : MonoBehaviour
     Vector2 mVelocity;
     Vector2 mDeltaVelocity;
     bool mGrounded;
-    float mPlayerHeight;
-	float mPlayerWidth;
+
+    Vector2 mPlayerSize;
+    Vector2 mHalfPlayerSize;
 
     // Use this for initialization
     void Start()
     {
         mRigidBody = GetComponent<Rigidbody2D>();
 
-		mPlayerHeight = GetComponent<SpriteRenderer> ().bounds.size.y;
-		mPlayerWidth = GetComponent<SpriteRenderer> ().bounds.size.x;
-        
+        mPlayerSize = new Vector2();
+        mPlayerSize.y = GetComponent<SpriteRenderer>().bounds.size.y;
+        mPlayerSize.x = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        mHalfPlayerSize = new Vector2();
+        mHalfPlayerSize.y = mPlayerSize.y / 2f;
+        mHalfPlayerSize.x = mPlayerSize.x / 2f;
+
         Fall();
     }
 
@@ -61,49 +67,39 @@ public class PlayerController : MonoBehaviour
         {
             if (mWalkingLeft)
             {
-				
-				Vector2 newPoint = new Vector2 (pos.x - MoveSpeed * Time.deltaTime, pos.y);
-
-				float width = newPoint.x - pos.x + playerWidth;
-				float height = playerHeight;
-
-				Vector2 newSize = new Vector2 (width, height);
-
-				float angle = 0;
-
-				int layerMask = CollisionLayer.value;
-
-				if (!Physics2D.OverlapBox(newPoint, newSize, angle, layerMask)) {
-					
-					pos.x = newPoint.x;
-					scale.x = -1;
-				}
+                mVelocity.x = -MoveSpeed;
+				scale.x = -1;
             }
 
             if (mWalkingRight)
             {
-                
-				Vector2 newPoint = new Vector2 (pos.x + MoveSpeed * Time.deltaTime, pos.y);
+                mVelocity.x = MoveSpeed;
+                scale.x = 1;
 
-				float width = newPoint.x - pos.x + playerWidth;
-				float height = playerHeight;
+                /*Vector2 newPoint = new Vector2(mPosition.x + MoveSpeed * Time.deltaTime, mPosition.y);
 
-				Vector2 newSize = new Vector2 (width, height);
+                float width = newPoint.x - mPosition.x + mPlayerWidth;
+                float height = mPlayerHeight;
 
-				float angle = 0;
+                Vector2 newSize = new Vector2(width, height);
 
-				int layerMask = CollisionLayer.value;
+                float angle = 0;
 
-				if (!Physics2D.OverlapBox(newPoint, newSize, angle, layerMask)) {
+                int layerMask = CollisionLayer.value;
 
-					pos.x = newPoint.x;
-					scale.x = 1;
-				}
+                if (!Physics2D.OverlapBox(newPoint, newSize, angle, layerMask))
+                {
+
+                    mPosition.x = newPoint.x;
+                    scale.x = 1;
+                }*/
             }
         }
 
         mDeltaVelocity.x = mVelocity.x * Time.deltaTime;
-        mPosition.x += mVelocity.x * Time.deltaTime;
+        HandleWallCollision();
+
+        mPosition.x += mDeltaVelocity.x;
 
         // Handle jumping and falling
         CheckTopBottomCollisions();
@@ -135,13 +131,55 @@ public class PlayerController : MonoBehaviour
         mGrounded = false;
     }
 
+    void HandleWallCollision()
+    {
+        Vector2 midWalkPoint = new Vector2(mPosition.x + (mDeltaVelocity.x / 2f), mPosition.y);
+        Vector2 boxSize = new Vector2(Mathf.Abs(mDeltaVelocity.x) + mPlayerSize.x, mPlayerSize.y);
+
+        DrawDebugBox(midWalkPoint, boxSize);
+
+        //Collider2D collisions = Physics2D.OverlapBox(midWalkPoint, boxSize, 0f, CollisionLayer);
+
+        Vector2 direction = new Vector2(mDeltaVelocity.x > 0f ? 1f : -1f, 0f);
+        //RaycastHit2D[] collisionResults = new RaycastHit2D[5];
+        //int collisionCount = Physics2D.BoxCastNonAlloc(mPosition, mPlayerSize, 0f, direction, collisionResults, mVelocity.x, CollisionLayer);
+
+        RaycastHit2D[] collisionResults = Physics2D.BoxCastAll(mPosition, mPlayerSize, 0f, direction, mDeltaVelocity.x, CollisionLayer);
+        if (collisionResults.Length > 0)
+        {
+            Debug.Log(collisionResults.Length);
+            for(int index = 0; index < collisionResults.Length; index++)
+            {
+                float yDistance = collisionResults[index].point.y - mPosition.y;
+                float xSign = collisionResults[index].point.x < mPosition.x ? -1 : 1;
+                if ((Mathf.Abs(yDistance) < (mHalfPlayerSize.y - 0.0001f)) && (xSign == direction.x))
+                {
+                    Debug.Log("WALL COLLISIONS!");
+                    mDeltaVelocity.x = mVelocity.x = 0f;
+                    mPosition.x = collisionResults[index].point.x - (mHalfPlayerSize.x * direction.x);
+                    break;
+                }
+            }
+            /*if (mVelocity.x != 0f)
+            {
+                Debug.Log("Size: " + mPlayerSize.x + ", " + mPlayerSize.y);
+                Debug.Log("Point: " + collisions.point.x + ", " + collisions.point.y);
+                //Debug.Log(collisions.point);
+                mVelocity.x = 0f;
+                mPosition.x = collisions.point.x + (mHalfPlayerSize.x * direction.x);
+                //mPosition.x = collisions.bounds.ClosestPoint(mPosition).x + (mPlayerWidth / 2f);
+                //Debug.Log(collisions.bounds.ClosestPoint(mPosition).y);
+            }*/
+        }
+    }
+
     void HandleGroundCollisions()
     {
         Vector2 velocity;
-        velocity.y = mVelocity.y < 0f ? mDeltaVelocity.y : -mHeight;
+        velocity.y = mVelocity.y < 0f ? mDeltaVelocity.y : -mPlayerSize.y;
 
         Vector2 midFallPoint = new Vector2(mPosition.x, mPosition.y + (velocity.y / 2f));
-        Vector2 boxSize = new Vector2(mWidth, Mathf.Abs(velocity.y) + mHeight);
+        Vector2 boxSize = new Vector2(mPlayerSize.x, Mathf.Abs(velocity.y) + mPlayerSize.y);
 
         Collider2D collisions = Physics2D.OverlapBox(midFallPoint, boxSize, 0f, CollisionLayer);
 
@@ -151,7 +189,7 @@ public class PlayerController : MonoBehaviour
             if (mVelocity.y < 0f)
             {
                 mVelocity.y = 0f;
-                mPosition.y = collisions.bounds.ClosestPoint(mPosition).y + mHeight/2f;
+                mPosition.y = collisions.bounds.ClosestPoint(mPosition).y + mHalfPlayerSize.y;
             }
         }
         else
